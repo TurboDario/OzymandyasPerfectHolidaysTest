9993be986abfef9c79e8eda810dedaa9f669f074
TurboDario <dario.diaz.guirado@gmail.com>
2023-05-08 20:14:50 +0200

    delete unnecesary files

--- weather_api/weather.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def get_weather_data(city, api_key):
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
-    response = requests.get(url)
-    return response.json()
